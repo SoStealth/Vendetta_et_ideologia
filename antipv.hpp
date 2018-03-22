059df3382365d3a18e75db41a56ba65931c920b9
SoStealth <36263330+SoStealth@users.noreply.github.com>
2018-03-22 10:48:37 +0000

    Update antipv.hpp

--- antipv.hpp
+++ antipv.hpp
@@ -1,1 +1,15 @@
-
+class AntiPV{
+private:Dictionary* userids;
+		Dictionary* passwds;
+public:	AntiPV(char*,char*);
+		~AntiPV();
+		void crack(Address*);
+};
+AntiPV::AntiPV(char* users, char* passwords) {
+	userids = new Dictionary(users);
+	passwds = new Dictionary(passwords);
+}
+AntiPV::~AntiPV() {
+	delete userids;
+	delete passwds;
+}
